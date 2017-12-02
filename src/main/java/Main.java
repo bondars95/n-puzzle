1c56e5443a2e8ea2dc8e6d26309186463035ddd6
dtelega <dtelega@MEGATHRON>
2017-12-02 14:03:43 +0200

    fix directory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -95,7 +95,7 @@ public class Main {
      * @throws IOException
      */
     private static byte[] readUsingFiles(String fileName) throws IOException {
-        fileName = "src/java/" + fileName;
+        fileName = "/home/dtelega/" + fileName;
         Path path = Paths.get(fileName);
 
         byte[] map = Files.readAllBytes(path);
@@ -109,7 +109,7 @@ public class Main {
     // number of movers to goal from initial state
     // sequence of states to goal
     public static void main(String[] args) throws IOException {
-        byte[] map = readUsingFiles(args[0]);
+ //       byte[] map = readUsingFiles(args[0]);
 
 //          byte[] map = generateMap(3);
 //        byte[] map = new byte[]{
